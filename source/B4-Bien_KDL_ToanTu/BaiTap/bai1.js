6026e7d3bc2fcecdb375aeab00b5b52b949f353f
BaoLopTruong <ngocbaole1999@gmail.com>
2021-12-09 22:23:32 +0700

    bai4-bt1- hien thi ngay thang nam

---
+++ source/B4-Bien_KDL_ToanTu/BaiTap/bai1.js
@@ -0,0 +1,5 @@
+let day =  parseInt(prompt("Nhập ngày sinh của bạn ở đây"));
+let month = parseInt(prompt("Nhập tháng sinh của bạn ở đây"));
+let year = parseInt(prompt("Nhập năm sinh của bạn ở đây"));
+
+alert("Ngày tháng năm sinh của bạn là : " + day + "-" + month +"-" + year);
