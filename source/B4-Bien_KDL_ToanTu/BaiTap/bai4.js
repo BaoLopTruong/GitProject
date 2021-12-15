67223d6ead5357ffa49de97063799186738bc4e8
BaoLopTruong <ngocbaole1999@gmail.com>
2021-12-15 21:52:16 +0700

    bai4-bt4- chuong trinh in noi dung buc thu

--- source/B4-Bien_KDL_ToanTu/BaiTap/bai4.js
+++ source/B4-Bien_KDL_ToanTu/BaiTap/bai4.js
@@ -1,22 +1,43 @@
-let name =  parseInt(prompt("Write the recipient's name here"));
-let add = parseInt(prompt("write the location here"));
-let time = parseInt(prompt("write time here"));
+var names =  prompt("Write the recipient's name here");
+let add = prompt("write the location here");
+let time = prompt("write time here");
 
-document.write('<p style= "font-style: italic"> " Joe thương nhớ,  <br> <br>');
+document.write("Tên người nhận: " + names + "<br>");
+document.write("Địa điểm: " + add + "<br>");
+document.write("Thời gian: " + time + "<br>");
+function taothu(){
 
-document.write("Em không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. " +
-"Em yêu anh đến khi trái tim này tan thành nghìn mảnh." + 
-"<br>Tất cả những gì em yêu thương, em khát khao và cần đến, chính là anh, mãi mãi về sau." + 
-"Em chỉ muốn ở bên anh, và anh yêu hỡi,<br>  em sẽ trở thành người phụ nữ như anh mong muốn. <br> <br>");
+// document.getElementById('names').innerHTML= "Tên người nhận: " + names;
+// document.getElementById('adds').innerHTML= "Địa điểm: " + add;
+// document.getElementById('times').innerHTML= "Thời gian: " + time;
 
-document.write("Có phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống?"+ 
-"Em hứa sẽ sẽ cố gắng triệu triệu<br>  lần hơn thế. Nhưng hơn tất cả, em chỉ mong một ngày nào đó, anh sẽ tự hào về em,"+
- "như tự hào về vợ của anh, và mẹ của các<br> con anh (ít nhất là 2 nhé, em vừa mới quyết định đấy!)."+
-  "Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong<br> vòng tay." +
-  "Đêm nay, em chỉ muốn được gần bên anh… và anh biết không, trái tim em đang đau đớn đến nhường nào. <br> <br>");
-
-  document.write("Anh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều. <br> <br>");
-  document.write("Los Angeles, ngày 10 tháng 10 năm 1951.”");
+ document.getElementById('letter').style.display='block';
 
 
-  document.write('</p>');
+}
+
+
+// let name =  parseInt(prompt("Write the recipient's name here"));
+// let add = parseInt(prompt("write the location here"));
+// let time = parseInt(prompt("write time here"));
+
+// document.write('<p style= "font-style: italic"> " Joe thương nhớ,  <br> <br>');
+
+// document.write("Em không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. " +
+// "Em yêu anh đến khi trái tim này tan thành nghìn mảnh." + 
+// "<br>Tất cả những gì em yêu thương, em khát khao và cần đến, chính là anh, mãi mãi về sau." + 
+// "Em chỉ muốn ở bên anh, và anh yêu hỡi,<br>  em sẽ trở thành người phụ nữ như anh mong muốn. <br> <br>");
+
+// document.write("Có phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống?"+ 
+// "Em hứa sẽ sẽ cố gắng triệu triệu<br>  lần hơn thế. Nhưng hơn tất cả, em chỉ mong một ngày nào đó, anh sẽ tự hào về em,"+
+//  "như tự hào về vợ của anh, và mẹ của các<br> con anh (ít nhất là 2 nhé, em vừa mới quyết định đấy!)."+
+//   "Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong<br> vòng tay." +
+//   "Đêm nay, em chỉ muốn được gần bên anh… và anh biết không, trái tim em đang đau đớn đến nhường nào. <br> <br>");
+
+//   document.write("Anh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều. <br> <br>");
+//   document.write("Los Angeles, ngày 10 tháng 10 năm 1951.”");
+
+
+//   document.write('</p>');
+
+
