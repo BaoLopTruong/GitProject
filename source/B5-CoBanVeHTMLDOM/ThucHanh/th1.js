8d22fa0672a3f4e84ddd1a637c1fd34641ca4d34
BaoLopTruong <ngocbaole1999@gmail.com>
2021-12-17 20:40:06 +0700

    bai5-th1-Hien Thi Dau Ra

---
+++ source/B5-CoBanVeHTMLDOM/ThucHanh/th1.js
@@ -0,0 +1,20 @@
+function showMessage(){
+
+    alert("Xin Chào");
+}
+
+function showMessage2(){
+
+    document.getElementById('txt2').innerHTML= "Xin Chào 2";
+}
+
+
+function showMessage3(){
+
+    document.write("Xin Chào 3");
+}
+
+function showMessage4(){
+
+    console.log("Xin Chào 4");
+}
