9bcb003caf8a45eb1a79bf28967132da35d630dc
BaoLopTruong <ngocbaole1999@gmail.com>
2021-12-22 15:35:57 +0700

    bai5-bt3-Kiem tra chan le cua mot so

---
+++ source/B5-CoBanVeHTMLDOM/BaiTap/bai3.js
@@ -0,0 +1,9 @@
+function chanle(){
+    let a = document.getElementById('input').value;
+    if(a % 2 == 0){
+        alert(a + " là số chẵn");
+    }
+    else{
+        alert(a + " là số lẻ");
+    }
+}
