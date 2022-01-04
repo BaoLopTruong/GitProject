b31a27d9c2294be6f568a5e97d1deb1643d1d42b
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-04 20:51:05 +0700

    bai7-th5-Thuc hanh vung oi mo ra

--- source/B7-CauTrucLap/ThucHanh/th5.js
+++ source/B7-CauTrucLap/ThucHanh/th5.js
@@ -1,12 +1,18 @@
 function vungoifunction(){
     let pass ;
     let thanchu = "vungoimora";
+    
     do{
         pass = document.getElementById('thanchu').value;
         alert("Thần chú sai rồi.");
-    }while(pass !== thanchu);
-    alert("Mở cửa thành công");
-    document.write('<span> 40 ten cuop da mo cua kho bau thanh cong. </span>');
+        break;
+    }while(pass != thanchu);
+    
+        
+        alert("Mở cửa thành công");
+        document.write('<span> 40 ten cuop da mo cua kho bau thanh cong. </span>');
+    
+   
 }
 
 
