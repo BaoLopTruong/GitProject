d737619d2715a55c767cd3034599b479d22fac20
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-05 15:34:06 +0700

    bai7-bt2-Day fibonacci

--- source/B7-CauTrucLap/BaiTap/bai2.js
+++ source/B7-CauTrucLap/BaiTap/bai2.js
@@ -1,19 +1,20 @@
+function myfunction(){
 
-   // let num = parseInt(document.getElementById('input').value);
-    let num1 = parseInt(prompt("nhập một số bất kỳ"));
+    let num = parseInt(document.getElementById('input').value);
+    //let num1 = parseInt(prompt("nhập một số bất kỳ"));
     let a =0, b = 1, temp;
-    document.write("Dãy fibonacci:");
-    while (num1 >= 0){
+    document.getElementById('kqua').innerHTML = "Dãy fibonacci: ";
+    while (num >= 0){
         
         temp = a;
         a = a + b;
         b = temp;
          
-        num1--;
+        num--;
         console.log(b);
-        document.write( b + " ");
+        document.getElementById('kqua').innerHTML += b + " ";
        
-       // alert(b);
+    
      
-      }
-  
+    } 
+}
