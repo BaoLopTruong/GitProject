d862ce5ab3d067aed666befc3b9a3100e426d953
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-05 15:27:18 +0700

    bai7-bt7-Hien thi day so cac so nguyen to

--- source/B7-CauTrucLap/BaiTap/bai7.js
+++ source/B7-CauTrucLap/BaiTap/bai7.js
@@ -2,24 +2,17 @@ function myfunction(){
     var input = document.getElementById('input').value;
     var count =0;
     var N = 2;
-    document.write("Result: " );
+
     while(count < input){
         if(N % 2 != 0){
            
             console.log(N + " ");
-            document.write (N + " ");
-            //document.getElementById('kqua').innerHTML = N + ", ";
+         
+            document.getElementById('kqua').innerHTML += N + ", ";
+            count ++;
         }
-        count ++;
+        
         N++;
     }
 
-    // for(;count < input ; count++){
-    //     if(N % 2 != 0){
-          
-    //         document.getElementById('kqua').innerHTML = "Kết quả" + N + ", ";
-           
-    //     }
-    //     N++;
-    // }
 }
