7ab65cc8a26ba064cd11932c03b49ee75a4f0b12
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-05 21:10:48 +0700

    bai7-bt4-Bang cuu chuong

---
+++ source/B7-CauTrucLap/BaiTap/bai4.js
@@ -0,0 +1,15 @@
+let html = "";
+
+for(let i =2; i < 10; i++){
+    
+    html +="<tr>";
+    for(let j = 1; j <=10; j++){
+        html += "<td>";
+        html += j + " * " + i + " =" + j*i;
+        html += "</td>";
+    }
+    
+    html += "</tr>";
+}
+
+//document.getElementById('bangcuuchuong').innerHTML = html;
