a4ed7f6eeefaa9654d89a0cbc8a5f5c669c95661
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-06 13:30:44 +0700

    bai7-bt5-Ve tam giac

--- source/B7-CauTrucLap/BaiTap/bai5.js
+++ source/B7-CauTrucLap/BaiTap/bai5.js
@@ -40,18 +40,44 @@ function dayfunction(){
 function phaifunction(){
     var N = 5;
     document.getElementById('kqua3').innerHTML = "<p> Kết quả <br> ";
-    for(i=0;i<N;i++) {
-        		//tao hang (row) *
-        		for(j=i;j>=0;j--) {
-        			//document.writeln(" * ");
-                    
-                    document.getElementById('kqua3').innerHTML += " * ";
+    for(i=N;i>0;i--) {
 
-        		}
-        		//xuong dong khi hien thi ket qua cua tung hang
-        		//document.writeln("<br/>");
-                document.getElementById('kqua3').innerHTML += "<br>";
-        	}
+            for(let j=0;j<i;j++){
+                //document.write("&nbsp;&nbsp;");
+               document.getElementById('kqua3').innerHTML += "&nbsp;&nbsp;";
+            }
+        		//tao hang (row) *
+        		for(let j=i;j<=N;j++) {
+                   document.getElementById('kqua3').innerHTML += "*";
+                    //document.write("*");
+    
+                }
+               // document.write("</br>");
+               document.getElementById('kqua3').innerHTML += "<br>";
+    }
+            document.getElementById('kqua3').innerHTML += "</p>";
+}
+
+
+
+function traifunction(){
+    var N = 5;
+    document.getElementById('kqua4').innerHTML = "<p> Kết quả <br> ";
+    for(i=1;i<=N;i++) {
+
+            for(let j=0;j<i;j++){
+                //document.write("&nbsp;&nbsp;");
+               document.getElementById('kqua4').innerHTML += "&nbsp;&nbsp;";
+            }
+        		//tao hang (row) *
+        		for(j=i;j<=N;j++) {
+                   document.getElementById('kqua4').innerHTML += "*";
+                    //document.write("*");
+    
+                }
+               // document.write("</br>");
+               document.getElementById('kqua4').innerHTML += "<br>";
+    }
             document.getElementById('kqua3').innerHTML += "</p>";
 }
 
