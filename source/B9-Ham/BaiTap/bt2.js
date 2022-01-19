de988a0904b8a666723394982a338d9f6926187b
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-19 16:37:28 +0700

    bai9=bt2-Ham kiem tra so nguyen to

--- source/B9-Ham/BaiTap/bt2.js
+++ source/B9-Ham/BaiTap/bt2.js
@@ -6,39 +6,42 @@ function checkinput(input){
     return true;
 }
 
+function kiemtrasnt(snt){
+    let flag = true;
+    if(snt == 2){
+        flag = true;
+        
+    }
+    else if(snt < 2){
+        flag = false;
+        document.getElementById('results').innerHTML =snt + " không là số nguyên tố";
+    }
+    else if(snt % 2 == 0){
+        flag = false;
+        
+    }
+    else{
+        for (let i = 3; i < Math.sqrt(snt); i+=2)
+        {
+            if (snt % i == 0){
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if (flag == true){
+        document.getElementById('results').innerHTML = snt + " là số nguyên tố";
+    }
+    else{
+    document.getElementById('results').innerHTML =snt + " không là số nguyên tố";
+    }
+}
 function checksnt(){
     let snt = document.getElementById('input').value;
     if(checkinput(snt)){
-        let flag = true;
-        if(snt == 2){
-            flag = true;
-            
-        }
-        else if(snt < 2){
-            flag = false;
-            document.getElementById('results').innerHTML =snt + " không là số nguyên tố";
-        }
-        else if(snt % 2 == 0){
-            flag = false;
-            
-        }
-        else{
-            for (let i = 3; i < Math.sqrt(snt); i+=2)
-            {
-                if (snt % i == 0){
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-        if (flag == true){
-            document.getElementById('results').innerHTML = snt + " là số nguyên tố";
-        }
-        else{
-        document.getElementById('results').innerHTML =snt + " không là số nguyên tố";
-        }
-   
+      
+        kiemtrasnt(snt);
     }
     else{
         alert("Vui lòng nhập giá trị 0 < n < 1000");
