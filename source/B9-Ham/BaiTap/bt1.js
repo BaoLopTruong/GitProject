2f3cbe38abf98d770209708962858fbf0e8117c2
BaoLopTruong <ngocbaole1999@gmail.com>
2022-01-19 20:22:17 +0700

    bai9=bt1-Chuyen doi giua feet va meters

--- source/B9-Ham/BaiTap/bt1.js
+++ source/B9-Ham/BaiTap/bt1.js
@@ -45,5 +45,52 @@ let getInput = parseFloat( document.getElementById('input').value);
 let gets = document.getElementById('choose').value;
 console.log(gets);
 
+function checkinput(value){
+   if(value < 0 ){
+      return false;
+   }
+   return true;
+}
 
+function Converts(){
+    var value = parseFloat(document.querySelector("#amount").value);
+    var change = document.getElementById("sl2").value;
+    var current = document.getElementById("sl1").value;
+  
+    console.log(value + change + current);
 
+if(current== "meters"){
+    switch(change){
+    case "feet": 
+        let S= MetersToFeet(value);
+        console.log(S);
+        document.getElementById('kqua').innerHTML = S ;
+        break;
+    
+    case "meters":
+        let S = value;
+        document.getElementById('kqua').innerHTML = S ;
+        break;
+        
+    }
+}
+else if(current == "feet"){
+
+    switch(change){
+        case "feet": 
+            let S = value;
+            document.getElementById('kqua').innerHTML = S ;
+            break;
+        
+        case "meters":
+            let S= FeetToMeters(value);
+            console.log(S);
+            document.getElementById('kqua').innerHTML = S ;
+            break;
+          
+            break;
+            
+        }
+
+}
+}
