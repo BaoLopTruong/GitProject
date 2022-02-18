58d8a4b0f72e56a7ea63f8441e6864f49b4e4d1a
BaoLopTruong <ngocbaole1999@gmail.com>
2022-02-18 20:45:26 +0700

    TestCase- Game Tetris

--- source/TetrisGame/JS/Piece.js
+++ source/TetrisGame/JS/Piece.js
@@ -1,44 +1,3 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
-const ROW = 18; // số cột
-const COL = 10; // số hàng
-const SQ = 40; // kích thước của một ô
-const COLOR = "WHITE"; // color của ô
-
-
-
-function drawSquare(x, y, color){
-    // chọn màu cho hình
-    ctx.fillStyle = color;
-    ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
-    // hình vuống thứ 2
-    ctx.strokeStyle = '#524A4E';
-    ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
-}
-
-// tạo mảng 2 chiều chứa gameboard
-
-let board = [];
-for(let r =0; r< ROW; r++){
-    board[r] = [];
-    for(let c = 0; c < COL; c++){
-        board[r][c] = COLOR;
-    }
-}
-//console.log(board);
-
-function drawBoard(){
-    for(let r =0; r< ROW; r++){
-        for(let c = 0; c < COL; c++){
-           drawSquare(c, r, board[r][c]);
-        }
-    }
-}
-
-drawBoard();
-
-// start
 
 class Piece{
     constructor(tetromino, color){
@@ -100,6 +59,7 @@ class Piece{
     }
 
     lock(){
+       
         for(let r = 0; r < this.activeTetromino.length; r++){
             for(let c = 0; c < this.activeTetromino.length; c++){
                 if(!this.activeTetromino[r][c]){
@@ -117,6 +77,30 @@ class Piece{
 
             }
         }
+
+        // xu ly tinh diem
+        for(let r = 0; r < ROW; r++){
+            let isFull =true;
+            for(let c = 0; c < COL; c++){
+                isFull = isFull && (board[r][c] != COLOR);
+            }
+
+            if(isFull){
+                for(let y = r; y > 1; y--){
+                    for( let c = 0; c < COL; c++){
+                        board[y][c] = board[y -1][c];
+                    }
+                }
+                for(let c= 0; c < COL; c++){
+                    board [0][c] = COLOR;
+                }
+
+                score += 10;
+            }
+        }
+        console.log(score);
+        drawBoard();
+        //document.querySelector('#score').innerText = sorce;
     }
 
     rotate(){
@@ -170,53 +154,3 @@ class Piece{
     }
 
 }
-
-// the end
-const PIECES = [
-    [Z, "red"],
-    [S, "green"],
-    [T, "yellow"],
-    [O, "blue"],
-    [L, "purple"],
-    [I, "cyan"],
-    [J, "orange"],
-];
-
-function randomPiece(){
-    let r = Math.floor(Math.random() * PIECES.length);
-    return new Piece(PIECES[r][0],PIECES[r][1] );
-}
-
-let p = randomPiece();
- console.log(p)
-
- // viest sự kiện click phím mũi tên
-document.addEventListener('keydown', function(e){
-    if(e.keyCode == 37){
-        p.moveLeft();
-    }
-    else if(e.keyCode == 39){
-        p.moveRight();
-    }
-    else if(e.keyCode == 38){
-        p.rotate();
-    }
-    else if(e.keyCode == 40){
-        p.moveDown();
-    }
-})
-
-
-let gameOver = false;
-let interval;
-
-function drop(){
-    interval = setInterval(function(){
-        if(! gameOver){
-            p.moveDown();
-        }else{
-            clearInterval(interval);
-        }
-    }, 1000)
-}
-drop();
---
+++ source/TetrisGame/JS/main.js
@@ -0,0 +1,90 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const ROW = 18; // số cột
+const COL = 10; // số hàng
+const SQ = 40; // kích thước của một ô
+const COLOR = "WHITE"; // color của ô
+
+let score =0;
+
+
+function drawSquare(x, y, color){
+    // chọn màu cho hình
+    ctx.fillStyle = color;
+    ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
+    // hình vuống thứ 2
+    ctx.strokeStyle = '#524A4E';
+    ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
+}
+
+// tạo mảng 2 chiều chứa gameboard
+
+let board = [];
+for(let r =0; r< ROW; r++){
+    board[r] = [];
+    for(let c = 0; c < COL; c++){
+        board[r][c] = COLOR;
+    }
+}
+//console.log(board);
+
+function drawBoard(){
+    for(let r =0; r< ROW; r++){
+        for(let c = 0; c < COL; c++){
+           drawSquare(c, r, board[r][c]);
+        }
+    }
+}
+
+drawBoard();
+
+
+const PIECES = [
+    [Z, "red"],
+    [S, "green"],
+    [T, "yellow"],
+    [O, "blue"],
+    [L, "purple"],
+    [I, "cyan"],
+    [J, "orange"],
+];
+
+function randomPiece(){
+    let r = Math.floor(Math.random() * PIECES.length);
+    return new Piece(PIECES[r][0],PIECES[r][1] );
+}
+
+let p = randomPiece();
+ console.log(p)
+
+ // viest sự kiện click phím mũi tên
+document.addEventListener('keydown', function(e){
+    if(e.keyCode == 37){
+        p.moveLeft();
+    }
+    else if(e.keyCode == 39){
+        p.moveRight();
+    }
+    else if(e.keyCode == 38){
+        p.rotate();
+    }
+    else if(e.keyCode == 40){
+        p.moveDown();
+    }
+})
+
+
+let gameOver = false;
+let interval;
+
+function drop(){
+    interval = setInterval(function(){
+        if(! gameOver){
+            p.moveDown();
+        }else{
+            clearInterval(interval);
+        }
+    }, 1000)
+}
+drop();
